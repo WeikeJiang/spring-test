69e1ae10024bfb88ff9da9875fb28f0d31d757e6
WeikeJiang <jiangweike@gmail.com>
2018-06-20 22:27:12 -0400

    add request param for post

--- src/main/java/com/demo/springbootdemo/web/HelloControler.java
+++ src/main/java/com/demo/springbootdemo/web/HelloControler.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class HelloControler {
 
 	@RequestMapping("/say")
@@ -23,5 +26,27 @@ public class HelloControler {
 		map.put("age", 18);
 
 		return map;
+	}
+
+	@GetMapping("/books/{id}")
+	public Object getOne(@PathVariable long id) {
+		System.out.println(" --- id: " + id);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "hello");
+		map.put("age", 18);
+		return map;
+	}
+	
+	@PostMapping("/books")
+	public Object post (@RequestParam String name,
+			            @RequestParam String author,
+			            @RequestParam String isbn) {
+		Map<String, Object> book = new HashMap<String, Object>();
+		book.put("name", name);
+		book.put("author", author);
+		book.put("isbn", isbn);
+		
+		return book;
 	}
 }
