de3c491583afe62abac7a8b9d1af556ef3358197
WeikeJiang <jiangweike@gmail.com>
2018-06-23 21:29:29 -0400

    test new yml file

--- src/main/java/com/demo/springbootdemo/web/HelloControler.java
+++ src/main/java/com/demo/springbootdemo/web/HelloControler.java
@@ -3,6 +3,7 @@ package com.demo.springbootdemo.web;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloControler {
 
+	@Value("${book.name}")
+	private String name;
+	@Value("${book.author}")
+	private String author;
+	@Value("${book.isbn}")
+	private String isbn;
+	
 	@RequestMapping("/say")
     public String hello() {
     	return "Hello spring boot!";
@@ -28,7 +36,7 @@ public class HelloControler {
 		return map;
 	}
 
-	@GetMapping("/books/{id}")
+	/*@GetMapping("/books/{id}")
 	public Object getOne(@PathVariable long id) {
 		System.out.println(" --- id: " + id);
 
@@ -36,6 +44,15 @@ public class HelloControler {
 		map.put("name", "hello");
 		map.put("age", 18);
 		return map;
+	}*/
+	
+	@GetMapping("/books/{id}")
+	public Object getOne(@PathVariable long id) {
+		Map<String, Object> book = new HashMap<>();
+		book.put("name", name);
+		book.put("author", author);
+		book.put("isbn", isbn);
+		return book;
 	}
 	
 	@PostMapping("/books")
